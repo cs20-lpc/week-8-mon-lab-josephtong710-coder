0244fcd372a0343d3ccd8968dd077a1740d5b307
Joseph Tong <josephtong710@gmail.com>
2026-03-11 20:06:04 -0700

    fixed

--- lab09-main.cpp
+++ lab09-main.cpp
@@ -4,76 +4,69 @@ using namespace std;
 
 template <typename T>
 void testRotate(ArrayStack<T>& s) {
-    cout << "\n-- Before rotate --\n" << s;
     s.rotate(Stack<T>::RIGHT);
-    cout << "-- After RIGHT rotate --\n" << s;
     s.rotate(Stack<T>::LEFT);
-    cout << "-- After LEFT rotate --\n" << s;
 }
 
 int main() {
-    ArrayStack<int> s(5);
-
-    // isEmpty
-    cout << "isEmpty: " << (s.isEmpty() ? "true" : "false") << endl;
-
-    // push
-    cout << "\nPushing 10, 20, 30, 40, 50...\n";
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(40);
-    s.push(50);
-    cout << s;
-
-    // isFull, getLength, getMaxSize
-    cout << "isFull: "     << (s.isFull() ? "true" : "false") << endl;
-    cout << "getLength: "  << s.getLength()  << endl;
-    cout << "getMaxSize: " << s.getMaxSize() << endl;
-
-    // peek
-    cout << "\npeek: " << s.peek() << endl;
-
-    // pop
-    cout << "\nPopping twice...\n";
-    s.pop();
-    s.pop();
-    cout << s;
-
-    // rotate
-    testRotate(s);
-
-    // copy constructor
-    cout << "\nCopy constructor:\n";
-    ArrayStack<int> s2(s);
-    cout << s2;
-
-    // assignment operator
-    cout << "Assignment operator:\n";
-    ArrayStack<int> s3(5);
-    s3 = s;
-    cout << s3;
-
-    // clear
-    cout << "Calling clear()...\n";
-    s.clear();
-    cout << s;
-    cout << "isEmpty after clear: " << (s.isEmpty() ? "true" : "false") << endl;
-
-    // exception handling
-    cout << "\n-- Exception tests --\n";
-    try { s.peek(); }
-    catch (string& e) { cout << "Caught: " << e << endl; }
-
-    try { s.pop(); }
-    catch (string& e) { cout << "Caught: " << e << endl; }
-
-    try {
-        ArrayStack<int> tiny(2);
-        tiny.push(1); tiny.push(2);
-        tiny.push(3);
+    // Stack A: push 0-9
+    ArrayStack<int> stackA(10);
+    for (int i = 0; i < 10; i++) {
+        stackA.push(i);
+        cout << i << " ";
     }
-    catch (string& e) { cout << "Caught: " << e << endl; }
+    cout << endl;
+
+    // isFull
+    if (stackA.isFull())
+        cout << "Stack A is full!" << endl;
+
+    // getLength
+    cout << "Stack A length: " << stackA.getLength() << endl;
+
+    // Copy constructor: Stack B = Stack A
+    ArrayStack<int> stackB(stackA);
+    cout << "Stack B length: " << stackB.getLength() << endl;
+
+    // push on full stack (exception)
+    try {
+        stackB.push(99);
+    } catch (string& e) {
+        cout << e << endl;
+    }
+
+    // pop 5 elements from Stack B, printing each
+    for (int i = 0; i < 5; i++) {
+        cout << "Popping " << stackB.peek() << " from stack B" << endl;
+        stackB.pop();
+    }
+    cout << "Stack B length: " << stackB.getLength() << endl;
+
+    // clear Stack B
+    stackB.clear();
+    cout << "Stack B is now empty after being cleared" << endl;
+
+    // pop on empty (exception)
+    try {
+        stackB.pop();
+    } catch (string& e) {
+        cout << e << endl;
+    }
+
+    // peek on empty (exception)
+    try {
+        stackB.peek();
+    } catch (string& e) {
+        cout << e << endl;
+    }
+
+    // Stack A still intact
+    cout << "Stack A length: " << stackA.getLength() << endl;
+
+    // Assignment operator: Stack C = Stack A
+    ArrayStack<int> stackC(10);
+    stackC = stackA;
+    cout << "Stack C length: " << stackC.getLength() << endl;
 
     return 0;
 }
